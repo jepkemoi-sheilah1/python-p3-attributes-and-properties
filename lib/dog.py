f4945efa8adcaa9b42b56ddf000a659c2cffbb7e
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-05-14 07:02:06 +0300

    completed attributes and properties lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,22 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self,name=None,breed=None):
+        if isinstance(name, str) and len(name) > 0 and len(name) < 25:
+            self.name = name
+        elif name is not None:
+            print("Name must be string between 1 and 25 characters.")
+
+        
+
+        if breed in APPROVED_BREEDS:
+            self.breed = breed
+        elif breed is not None:
+            print("Breed must be in list of approved breeds.")
+       
+    
+dog1 = Dog("Fido", "Pug")
+
+     
+        
+        
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,16 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self,name =None,job=None):
+        if isinstance(name, str) and len(name) > 0 and len(name) < 25:
+            self.name = name.title()
+        elif name is not None:
+            print("Name must be string between 1 and 25 characters.")
+
+    
+        if job in APPROVED_JOBS:
+            self.job = job
+        elif job is not None:
+              print("Job must be in list of approved jobs.")
+           
+person1 = Person('Memoo','ITC')
